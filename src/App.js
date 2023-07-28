6b5862d8c63b719ec978aafd2a4f74de31be3095
Jeric <marianovjeric@gmail.com>
2023-07-28 16:01:27 -0700

    finished featured and discounted books section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Discounted from "./components/Discounted";
+import Featured from "./components/Featured";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
@@ -8,6 +10,8 @@ function App() {
      <Nav />
      <Landing />
      <Highlights />
+     <Featured />
+     <Discounted />
     </div>
   );
 }
